45d22fc1cf98e143cdd5e530a68b84fdb7577a69
Wil Gunter <in.wil.i.trust@gmailcom>
2013-10-10 17:16:07 -0700

    Canvas

---
+++ src/components.js
@@ -0,0 +1,91 @@
+Crafty.c('Grid', {
+	init: function() {
+		this.attr({
+			w: Game.map_grid.tile.width,
+			h: Game.map_grid.tile.height
+		})
+	},
+
+	at: function(x, y) {
+		if (x === undefined && y === undefined) {
+			return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
+		} else {
+			this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height });
+			return this;
+		}
+	}
+});
+
+Crafty.c('Actor', {
+	init: function() {
+		this.requires('2D, Canvas, Grid');
+	},
+});
+
+Crafty.c('Life', {
+	init: function() {
+		this.life = 100;
+		this.takeDamage = function(damage) {
+			this.life -= damage;
+			if(this.life <= 0) {
+				this.trigger('Death', damage);
+			}
+		};
+	}
+});
+
+
+Crafty.c('Tree', {
+	init: function() {
+		this.requires('Actor, Color, Solid')
+		.color('rgb(20, 125, 40)');
+	},
+});
+
+Crafty.c('Bush', {
+	init: function() {
+		this.requires('Actor, Color, Solid')
+		.color('rgb(20, 185, 40)');
+	},
+});
+
+Crafty.c('PlayerCharacter', {
+	init: function() {
+		this.requires('Actor, Fourway, Color, Collision, Gravity')
+		.fourway(4)
+		.color('rgb(20, 75, 40)')
+		.gravity('Solid')
+		.stopOnSolids()
+		.onHit('Village', this.visitVillage);
+	},
+
+	stopOnSolids: function() {
+		this.onHit('Solid', this.stopMovement);
+		return this;
+	},
+
+	stopMovement: function() {
+		this._speed = 0;
+		if(this._movement) {
+			this.x -= this._movement.x;
+			this.y -= this._movement.y;
+		}
+	},
+
+	visitVillage: function(data) {
+		villlage = data[0].obj;
+		villlage.visit();
+	}
+});
+
+Crafty.c('Village', {
+	init: function() {
+		this.requires('Actor, Color')
+		.color('rgb(170, 125, 40)');
+	},
+
+	visit: function() {
+		this.destroy();
+		Crafty.trigger('VillageVisited', this);
+	}
+});
---
+++ src/game.js
@@ -0,0 +1,26 @@
+Game = {
+
+	map_grid: {
+		width:  42,
+		height: 32,
+		tile: {
+			width:  16,
+			height: 16
+		}
+	},
+
+	width: function() {
+		return this.map_grid.width * this.map_grid.tile.width;
+	},
+
+	height: function() {
+		return this.map_grid.height * this.map_grid.tile.height;
+	},
+
+	start: function() {
+		Crafty.init(Game.width(), Game.height());
+		Crafty.background('rgb(249, 223, 125)');
+
+		Crafty.scene('Game');
+	}
+}
