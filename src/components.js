ed509edac6fed916722c72cf9b6e219b35e71f2e
Wil Gunter <in.wil.i.trust@gmailcom>
2013-10-14 11:57:07 -0700

    Added enemies, fixed particles, fixed fire.

--- src/components.js
+++ src/components.js
@@ -1,3 +1,33 @@
+var options = {
+	maxParticles: 150,
+	size: 16,
+	sizeRandom: 0,
+	speed: 5,
+	speedRandom: 1.2,
+    // Lifespan in frames
+    lifeSpan: 29,
+    lifeSpanRandom: 7,
+    // Angle is calculated clockwise: 12pm is 0deg, 3pm is 90deg etc.
+    angle: 180,
+    angleRandom: 34,
+    startColour: [255, 131, 0, 1],
+    startColourRandom: [48, 50, 45, 0],
+    endColour: [245, 35, 0, 0],
+    endColourRandom: [60, 60, 60, 0],
+    // Only applies when fastMode is off, specifies how sharp the gradients are drawn
+    sharpness: 20,
+    sharpnessRandom: 10,
+    // Random spread from origin
+    spread: 5,
+    // How many frames should this last
+    duration: -1,
+    // Will draw squares instead of circle gradients
+    fastMode: true,
+    gravity: { x: 0, y: 1 },
+    // sensible values are 0-3
+    jitter: 0
+}
+
 Crafty.c('Grid', {
 	init: function() {
 		this.attr({
@@ -31,68 +61,95 @@ Crafty.c('Actor', {
 	}
 });
 
-Crafty.c('Controls', {
-	init: function() {
-		this.requires('Multiway')
-		.multiway( 3, { A: 180, D: 0 })
-		.enableControl();
-		this.bind('EventFrame', function() {
-			this.bind('KeyDown', function(e) {
-				if( e.key === 32 ) {
-					Crafty.e('Plasma').create( this.x, this.y  - 10 );
-				}
-			});
-		});
-	}
-});
-
 Crafty.c('Character', {
 	init: function() {
-		this.requires('Actor, Collision')
+		this.requires('Actor, Collision, Life')
 		.collision();
 	},
 	destory: function() {
 		this.destory();
+	},
+});
+
+Crafty.c('Life', {
+	init: function() {
+		this.life = 100;
+		this.alive = true;
+	},
+	getLife: function() {
+		return this.life;
+	},
+	takeDamage: function(damage) {
+		this.life -= damage;
+		if(this.life <= 0) {
+			this.alive = false;
+		}
+	},
+	isAlive: function() {
+		return this.alive;
+	}
+});
+
+Crafty.c('Controls', {
+	init: function() {
+		this.requires('Multiway')
+		.multiway( 5, { A: 180, D: 0 })
+		.enableControl();
+		this.bind('KeyDown', function(e) {
+			if( e.key === 32 ) {
+				Crafty.e('Plasma').create( this.x, this.y  - 10, 3);
+			}
+		});
 	}
 });
 
 Crafty.c('Plasma', {
 	init: function() {
-		this.requires('Actor, Collision, Solid, spr_plasma');
+		this.requires('Collision');
+		this.bind('EnterFrame', function(dt) {
+			this.y -= dt.dt / 8
+		});
+		this.onHit('Solid', function(hit) {
+			this.destroy();
+			hit[0].obj.takeDamage(10);
+			console.log(hit[0].obj.getLife());
+			// hit[0].takeDamage(10);
+		});
 	},
 	create: function(x, y, speed) {
+		this.addComponent( 'Actor, Collision, Solid, spr_plasma' )
 		this.attr({
 			x: x,
 			y: y,
 			w: Game.map_grid.tile.width,
 			h: Game.map_grid.tile.height
 		});
-		this.collision();
-		this.bind('EnterFrame', function(dt) {
-			this.y -= dt.dt / 8
-			console.log(this.y);
-		});
-		return this;
 	},
+	destory: function() {
+		this.destroy();
+	}
 });
 
-Crafty.c('Wall', {
-	init: function() {
-		this.requires('Actor, Solid, spr_wall');
-	},
-});
-
-Crafty.c('Floor', {
-	init: function() {
-		this.requires('Actor, Solid, spr_floor')
-		.collision();
-	},
-
-});
-
-Crafty.c('PlayerCharacter', {
+Crafty.c('Player', {
 	init: function() {
 		this.requires('Character, spr_player, Controls');
+		var pcParticles = Crafty.e("Actor, 2D,Canvas,Particles").particles(options);
+		this.bind('EnterFrame', function(dt) {
+			pcParticles.x = this.x;
+			pcParticles.y = this.y + 16;
+		});
 
+	},
+});
+
+Crafty.c('Enemy', {
+	init: function() {
+		this.requires('Character, spr_target, Solid');
+		console.log(this.getLife());
+		this.bind('EnterFrame', function() {
+			if(!this.isAlive()) {
+				this.destroy();
+			}
+		});
 	},
 });
--- src/scenes.js
+++ src/scenes.js
@@ -1,50 +1,12 @@
-Crafty.scene('Game', function() {
+Crafty.scene('LevelOne', function() {
 
 
+ // Player 
+ this.player = Crafty.e('Player').at(15, 35);
 
-  var options = {
-    maxParticles: 150,
-    size: 16,
-    sizeRandom: 0,
-    speed: 1,
-    speedRandom: 1.2,
-    // Lifespan in frames
-    lifeSpan: 29,
-    lifeSpanRandom: 7,
-    // Angle is calculated clockwise: 12pm is 0deg, 3pm is 90deg etc.
-    angle: 0,
-    angleRandom: 34,
-    startColour: [255, 131, 0, 1],
-    startColourRandom: [48, 50, 45, 0],
-    endColour: [245, 35, 0, 0],
-    endColourRandom: [60, 60, 60, 0],
-    // Only applies when fastMode is off, specifies how sharp the gradients are drawn
-    sharpness: 20,
-    sharpnessRandom: 10,
-    // Random spread from origin
-    spread: 10,
-    // How many frames should this last
-    duration: -1,
-    // Will draw squares instead of circle gradients
-    fastMode: false,
-    gravity: { x: 0, y: 1 },
-    // sensible values are 0-3
-    jitter: 0
-}
+ this.enemy = Crafty.e('Enemy').at(15,10);
 
-Crafty.e("Actor, 2D,Canvas,Particles").particles(options).at(15,36);
-
-
-  // Player 
-  this.player = Crafty.e('PlayerCharacter').at(15, 35);
-
-  Crafty.e("2D, DOM, FPS, Text")
-  .attr({maxValues:10})
-  .bind("MessureFPS",function(fps) { 
-    this.text("FPS"+fps.value );
-  });
 });
-
 
 Crafty.scene('Loading', function() {
 
@@ -59,14 +21,14 @@ Crafty.scene('Loading', function() {
 
     //load spritemap individually once they have loaded.
     Crafty.sprite(16, 'assets/16x16.png', {
-      spr_wall:  [0, 0],
+      spr_wall:       [0, 0],
       spr_background: [1, 0],
-      spr_target:[2, 0],
-      spr_player:[3, 0],
-      spr_plasma:[0, 1]
+      spr_target:     [2, 0],
+      spr_player:     [3, 0],
+      spr_plasma:     [0, 1],
+      spr_enemy:      [1, 1]
     });
 
-    Crafty.scene('Game');
-
+    Crafty.scene('LevelOne');
   })
 });
